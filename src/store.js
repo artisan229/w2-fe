58674364484ea84882d68b95fa7b84299920c8cf
artisan <kmgnikmgni@gmail.com>
2022-06-15 02:10:40 +0900

    user redux 추가(로그인 표시)

--- src/store.js
+++ src/store.js
@@ -12,8 +12,21 @@ const login = createSlice({
 
 export const { changeState } = login.actions;
 
+const user = createSlice({
+    name: 'user',
+    initialState: { value: {name: "", email: "", profile_img: ""}},
+    reducers: {
+        changeUser(state, action) {
+            state.value = action.payload
+        }
+    }
+})
+
+export const { changeUser } = user.actions;
+
 export default configureStore({
     reducer: {
-        login: login.reducer
+        login: login.reducer,
+        user: user.reducer,
     }
 })
